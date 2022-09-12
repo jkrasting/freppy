f4e0a533f2dcdf1293becd2c90d1445e3bac9541
John Krasting <John.Krasting@noaa.gov>
2022-09-12 15:24:44 -0400

    Updated __init__.py

--- freppy/__init__.py
+++ freppy/__init__.py
@@ -4,3 +4,5 @@ from .freppy import get_nc_files
 from .freppy import parse_date_string
 from .freppy import infer_attributes
 from .freppy import catalog_from_dir
+from .freppy import consolidate_monthly_av
+from .freppy import infer_av_variables
